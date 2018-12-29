7d483ed831767a48074693d73bb5dfdcbb4eec9b
RJ Dellecese <rj.dellecese@annkissam.com>
2018-12-28 21:59:59 -0500

    Add coveralls params to mix project config

--- apps/game/mix.exs
+++ apps/game/mix.exs
@@ -11,7 +11,15 @@ defmodule Game.MixProject do
       lockfile: "../../mix.lock",
       elixir: "~> 1.7.4",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.circle": :test
+      ]
     ]
   end
 
