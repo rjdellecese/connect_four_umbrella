eb8ab5a423e47a5173e2e82010e7f3ecdb14e946
rjdellecese <rjdellecese@gmail.com>
2020-11-05 01:07:06 +0000

    Use a bash script to set up

--- mix.exs
+++ mix.exs
@@ -6,7 +6,6 @@ defmodule ConnectFour.MixProject do
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases(),
 
       # Test
       test_coverage: [tool: ExCoveralls],
@@ -35,9 +34,5 @@ defmodule ConnectFour.MixProject do
       {:credo, "~> 1.5.0", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test}
     ]
-  end
-  
-  defp aliases do
-    [gitpod_setup: ["deps.get", "cmd 'cd apps/cli && mix escript.build'"]]
   end
 end
